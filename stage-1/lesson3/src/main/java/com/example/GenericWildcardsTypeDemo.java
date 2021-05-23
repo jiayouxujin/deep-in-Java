423c3b36b0ab7062e466714b93472170454fdaf7
xujin <1319039722@qq.com>
2021-05-23 19:10:35 +0800

    泛型设计的extends和super

--- stage-1/lesson3/src/main/java/com/example/GenericWildcardsTypeDemo.java
+++ stage-1/lesson3/src/main/java/com/example/GenericWildcardsTypeDemo.java
@@ -14,10 +14,22 @@ public class GenericWildcardsTypeDemo {
         upperBoundedWildcards(numbers);
     }
 
-    private static void lowerBoundedWildcards(List<Number> numbers){
-
+    private static void lowerBoundedWildcards(List<Number> numbers) {
+        lowerBoundedWildcardsDemo(numbers,numbers);
     }
-    private static void upperBoundedWildcards(List<Number> numbers){
+
+    private static void lowerBoundedWildcardsDemo(List<? extends Number> producer, List<? super Number> consumer) {
+        //读取数据（生产者）使用extends
+        //操作输出（消费者）试着super
+        for(Number number:producer){
+
+        }
+
+        consumer.add(1);
+        consumer.add((short)2);
+    }
+
+    private static void upperBoundedWildcards(List<Number> numbers) {
 
         numbers.add(Byte.valueOf((byte) 1));
         numbers.add(Short.valueOf((short) 2));
@@ -29,15 +41,17 @@ public class GenericWildcardsTypeDemo {
         numbers.addAll(bytes);
         numbers.addAll(shorts);
         //被操作的对象，需要更为抽象的类型，Number类似底层，然后可以整合它的上层东西(这是一种设计的方式)
-        forEach(numbers,System.out::println);
+        forEach(numbers, System.out::println);
 
         //完全通配
         println(numbers);
     }
+
     /**
      * 这里的两个?不是同一个东西
      * 通配符少用
-     *类型是object
+     * 类型是object
+     *
      * @param list
      * @param consumer
      */
@@ -50,10 +64,11 @@ public class GenericWildcardsTypeDemo {
     /**
      * 完全通配可以在运行的时候与其它的任意类型都会产生方法签名的冲突
      * 如果使用具体类型，在编译的时候会限制使用的类型
+     *
      * @param iterable
      */
-    public static void println(Iterable<?> iterable){
-        for(Object e:iterable){
+    public static void println(Iterable<?> iterable) {
+        for (Object e : iterable) {
             System.out.println(e);
         }
     }
