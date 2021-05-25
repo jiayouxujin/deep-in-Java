61ca170076253de7e1c776fe6f890c7dac166a13
xujin <1319039722@qq.com>
2021-05-25 21:16:16 +0800

    why function

---
+++ stage-1/functionlesson/src/main/java/ActionDemo.java
@@ -0,0 +1,16 @@
+public class ActionDemo {
+
+    public static void main(String[] args) {
+        Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        };
+
+
+        Runnable runnable1=()->{
+
+        };
+    }
+}
